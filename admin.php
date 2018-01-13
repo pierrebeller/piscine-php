75ef57fe5aa06c32b0bef626173ffadc1bb0dd94
Pierre BELLER <pbeller@e3r5p15.42.fr>
2018-01-13 14:19:00 +0100

    add 2

---
+++ admin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Page protégée par mot de passe</title>
+    </head>
+    <body>
+        <p>Mot de passe admin:</p>
+        <form action="secret.php" method="post">
+            <p>
+            <input type="password" name="mot_de_passe" />
+            <input type="submit" value="Valider" />
+            </p>
+        </form>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Minishop 42</title>
+</head>
+<body>
+
+<h1><center>Ft_minishop</center></h1>
+<br />
+<form action="create.php" method="POST">
+	Identifiant: <input type="text" name="login" />
+	<br />
+	Mot de passe: <input type="password" name="passwd" />
+	<br />
+	Submit <input type="submit" name="submit" value="OK"/>
+	<br />
+	
+</form>
+<form action="profil.php" method="POST">
+	Profil <input type="submit" name="submit" value="Profil"/>
+</form>
+
+
+</body>
+</html>
