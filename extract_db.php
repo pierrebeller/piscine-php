013074ee90c776baed473567ad7a25f6a02fa21e
Pierre BELLER <pbeller@e3r5p15.42.fr>
2018-01-13 12:31:31 +0100

    add extract_db func

---
+++ extract_db.php
@@ -0,0 +1,25 @@
+#!/usr/bin/php
+<?php
+function extract_db($path)
+{
+	$fd = fopen("./articles.csv", r);
+	$i  = 0;
+	while  ($tab = fgetcsv($fd))
+	{
+		$array[$i] = $tab;
+		$j = 0;
+		foreach ($array[$i] as $key => $value)
+		{
+			preg_match("/(^.*?):/", $value, $matches);
+			preg_match("/.*:(.*)/", $value, $matches_2);
+			$array[$i][$matches[1]] = $matches_2[1];
+			unset($array[$i][$j]);
+			$j++;
+	
+		}
+	
+		$i++;
+	}
+	return ($array);
+}
+?>
